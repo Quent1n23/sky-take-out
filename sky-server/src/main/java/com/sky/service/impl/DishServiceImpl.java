d1f1baae5dcb8a3844eeb3f322191e75c00ddfae
Quent1n23 <1020266436@qq.com>
2024-04-09 23:30:55 +0800

    (优化)删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -98,12 +98,19 @@ public class DishServiceImpl implements DishService {
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
 
-        //删除菜品表中的菜品数据
+        /*//删除菜品表中的菜品数据
         for (Long id : ids) {
             dishMapper.deleteById(id);
             //删除菜品关联的口味数据
             dishFlavorMapper.deleteByDishId(id);
-        }
+        }*/
+
+        //优化 sql:delete from dish where id in(?,?,?)
+        //优化 sql:delete from dish_flavor where dish_id in(?,?,?)
+        //根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+        //根据菜品id集合批量删除关联的口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
 
 
     }
